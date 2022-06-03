0272f1c2ee3cbe694a403ab8f9e4e94de50c3681
santiill <szebiniso0909@gmail.com>
2022-06-03 09:39:51 +0600

    commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,8 @@ import "../baseStyles.css";
 import s from "./Header.module.css";
 
 const Header = ({ openBar, bar }) => {
+  const role = JSON.parse(localStorage.getItem("token"));
+
   return (
     <div className={s.header_cont}>
       <p className={`${s.header_text} ${s.dn_title}`}>Grade system</p>
@@ -12,7 +14,11 @@ const Header = ({ openBar, bar }) => {
       >
         &#9776;
       </p>
-      <p className={s.header_text}>Hi, User ;)</p>
+      <p className={s.header_text}>
+        {role.is_superuser == true && "Hi, Super User"}
+        {role.is_student == true && "Hi, Akylai Temiralieva"}
+        {role.is_professor == true && "Hi, Ruslan Isaev"}
+      </p>
     </div>
   );
 };
